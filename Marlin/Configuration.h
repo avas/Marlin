a22590c8d1efb5b1d6f72eecc191a92ad1a8af8a
Andrey A. Vasenev <avas.the.wise@gmail.com>
2021-01-20 21:46:31 +0700

    Adjusted nozzle-to-probe offset for current Hemera mount.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -931,7 +931,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -55, -13, -0.65 }
+#define NOZZLE_TO_PROBE_OFFSET { -42, 0, -2.3 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
