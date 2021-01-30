2d23bbbedab2eebfb6711fb643a1b2652a70a8ed
Andrey A. Vasenev <avas.the.wise@gmail.com>
2021-01-30 11:36:48 +0700

    Adjusted probe Z offset, decreased probing accuracy to 1 measure/point - this should restore print preparation time almost to pre-dualZ value.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -985,7 +985,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42, 0, -2.75 }
+#define NOZZLE_TO_PROBE_OFFSET { -42, 0, -2.65 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1009,7 +1009,7 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-#define MULTIPLE_PROBING 2
+//#define MULTIPLE_PROBING 2
 //#define EXTRA_PROBING    1
 
 /**
